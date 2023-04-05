1a252fab8e117d82a89d7d056c2b1d30a7b15299
swholobaugh <seth.holobaugh@gmail.com>
2023-04-05 17:43:16 -0500

    complete initial setup

--- react-app/src/pages/Home/Index.tsx
+++ react-app/src/pages/Home/Index.tsx
@@ -2,12 +2,7 @@ import * as React from 'react';
 const Index = (): React.ReactNode => {
   return (
     <div className="flex flex-col gap-y-4 p-2">
-      <ul className="text-2xl">
-        <li><span className="font-semibold">D</span>jango</li>
-        <li><span className="font-semibold">I</span>nertia</li>
-        <li><span className="font-semibold">R</span>eact</li>
-        <li><span className="font-semibold">T</span>ailwind CSS</li>
-      </ul>
+      Hello Django/Inertia!
     </div>
   )
 }
