d3158f24c920e19f5758b7b2c29165951710f3ea
swholobaugh <seth.holobaugh@gmail.com>
2023-04-07 14:07:11 -0500

    feat(pages): add fe code

---
+++ react-app/src/pages/Blog/Index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+const Index = (): React.ReactNode => {
+  return (
+    <div className="flex flex-col gap-y-4 p-2">
+      Welcome to the Blog Page
+    </div>
+  )
+}
+
+export default Index;
