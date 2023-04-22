647a4a3e8b8e6c4ad3d988e914e305f6b6251552
swholobaugh <seth.holobaugh@gmail.com>
2023-04-22 11:06:30 -0500

    feat(navbar): add basic styles

--- react-app/src/components/navbar/Navbar.tsx
+++ react-app/src/components/navbar/Navbar.tsx
@@ -1,12 +1,29 @@
-import { Link } from '@inertiajs/react'
+import { Link, usePage } from '@inertiajs/react'
+
 
 function Navbar() {
+  const {component} = usePage()
 
   return (
     <div className="flex flex-row justify-center">
-      <Link className='mx-4' href="/">Home</Link>
-      <Link className='mx-4' href="/about">About</Link>
-      <Link className='mx-4' href="/blog">Blog</Link>
+      <Link
+        className={`mx-4 ${component === 'Home/Index' ? 'font-bold border-bottom border-b-2' : 'font-normal'}`}
+        href="/"
+      >
+        Home
+      </Link>
+      <Link
+        className={`mx-4 ${component === 'About/Index' ? 'font-bold border-bottom border-b-2' : 'font-normal'}`}
+        href="/about"
+      >
+        About
+      </Link>
+      <Link
+        className={`mx-4 ${component === 'Blog/Index' ? 'font-bold border-bottom border-b-2' : 'font-normal'}`}
+        href="/blog"
+      >
+        Blog
+      </Link>
     </div>
   )
 }
