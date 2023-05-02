1df416fc007609ea6e58ccec8ef7700911c93ed1
swholobaugh <seth.holobaugh@gmail.com>
2023-05-02 16:56:57 -0500

    feat(files): create script to add post file

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
+import uuid
 from django.db import models
 
 
 class Post(models.Model):
-    meta_universalid = models.UUIDField()
+    meta_universalid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file_name = models.CharField(max_length=150)
     created_date = models.DateField(auto_created=True)
 
